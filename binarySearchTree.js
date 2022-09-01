2c37fb97c219e0a6001eb620925f904f35bdfa0b
Matheus <matheuspl92@hotmail.com>
2022-09-01 16:21:03 -0300

    Add NodeFactory function

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -2,3 +2,10 @@
 *   Code written for the Binary Search Trees project assignment of The Odin Project.
 */
 
+const NodeFactory = (data) => {
+  let data = data;
+  let left = null;
+  let right = null;
+
+  return { data, left, right }
+};
